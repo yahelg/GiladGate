dd666fb1d99dd663235923d9512a0bbc54255c99
yahelg <yahelg@gmail.com>
2015-04-08 15:57:14 +0300

    Right before changing to the discrete way of error correction. Major changes will now take place in GiladGate3q.py

--- GiladGate3q.py
+++ GiladGate3q.py
@@ -4,7 +4,6 @@ from qutip import *
 from scipy import *
 from scipy.signal import argrelextrema
 from scipy.optimize import curve_fit
-out = dict()
 
 def operate(a,b):
     return vector_to_operator(a*operator_to_vector(b))
@@ -47,6 +46,28 @@ def printstates(b):
     states = [strket(eigstates[i].tidyup(0.001)) for i in range(len(eige)) if norm(eige[i])>0.001 ]
     ens = ['({0.real:.2f})'.format(eige[i]) for i in range(len(eige)) if norm(eige[i])>0.001]
     print '\n'.join(['%s : %s'%(i,j) for (i,j) in zip(ens,states)])
+
+def showdecay(a):
+    vals = real(expect(ketbra(L1),a.rho))
+    smoothed = np.convolve(vals, np.ones(10)/10)
+    maxi = argrelextrema(smoothed, np.greater)[0]
+    d = a.data
+    p0=(0.2,0.5*d.gamma*(d.g/d.delta)**2, 0.5)
+    popt, pcov = curve_fit(func, a.timelist[maxi],vals[maxi],p0)
+    plot(a.timelist,func(a.timelist,*popt))
+    print 'decayrate: ',popt[1],' vs gamma*(g/delta)^2: ',d.gamma*(d.g/d.delta)**2
+    return popt[1]
+
+def showperiod(a,points=10,maxed=5):
+    vals = real(expect(ketbra(L1),a.rho))
+    smoothed = np.convolve(vals, np.ones(points)/points)
+    maxi = argrelextrema(smoothed, np.greater)[0]
+    pr = 1/(a.timelist[maxi[3]] - a.timelist[maxi[2]])
+    d = a.data
+    print 'period: ',pr,' vs g^2/(4*delta): ',d.g**2/(4*d.delta)
+    plot(a.timelist[:maxi[maxed]],vals[:maxi[maxed]])
+    plot(a.timelist[maxi[[2,3]]],vals[maxi[[2,3]]],'r*')
+    return pr
     
 L1 = unify(ket('110')+ket('000'))
 L2 = unify(ket('111')-ket('001'))
@@ -69,14 +90,15 @@ class params(object):
         self.delta = 10
     def rho0(self):
         return ketbra(L1)
+    def H0(self):
+        return -self.delta*(ketbra(L1)+ketbra(L2))        
     def Ht(self):
         #Hamiltonian and changes
         #Might cause wrong error correction because L1 and L2 rotate
-        H0 = -self.delta*(ketbra(L1)+ketbra(L2))
         #H0 = self.delta*ketbra(L3)
         V = self.g/2 * (ketbra(L1,L3) + ketbra(L2,L3))
         V = V + V.dag()
-        return H0 + V
+        return self.H0() + V
     def Ld(self):
         ld = lindblad_dissipator(tensor(sigmam(),qeye(2),qeye(2)))
         # Add upwords dissipation
@@ -85,6 +107,8 @@ class params(object):
     def Lec(self):
         lec = sprepost(Pc,Pc)+ sprepost(U*Pe,Pe*U) -spre(I)
         return  self.gec * lec
+    def correct(self):
+        return sprepost(Pc,Pc)+ sprepost(U*Pe,Pe*U)
     def printParams(self):
         print 'g: %f\ngamma: %f\ngec: %f\ndelta: %f'%(self.g,self.gamma,self.gec,self.delta)
 
@@ -95,6 +119,12 @@ class paramsDecayAndCorrection(params):
         return self.gamma/2 * (sprepost(A1,A1.dag()) + sprepost(A2,A2.dag()) - 2*spre(I))
     def Lec(self):
         pass
+
+class paramsSigma(params):
+    def Ht(self):
+        V = self.g/2 * tensor(sigmaz(),qeye(2),qeye(2))
+        V += self.g/2 * tensor(qeye(2),qeye(2),sigmax())
+        return self.H0() + V
 
 class rhoResult(object):
     def __init__(self,data,rho,timelist=None):
